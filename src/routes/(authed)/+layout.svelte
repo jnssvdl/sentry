73fc0f0fca54f8a7b84f0c4152aebc9f94b2a249
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2026-01-12 20:27:12 +0800

    feat: separate admin and user layouts - Moved faculty and student routes into a new (portal) group - Relocated shared header/footer layout to (portal)/+layout.svelte - Separated admin UI from standard user interface while maintaining shared auth logic

--- src/routes/(authed)/+layout.svelte
+++ src/routes/(authed)/+layout.svelte
@@ -1,31 +1,5 @@
 <script lang="ts">
-	import ModeToggle from '$lib/components/ModeToggle.svelte';
-	import Button from '$lib/components/ui/button/button.svelte';
-	import { LogOut } from '@lucide/svelte';
-
 	let { children } = $props();
 </script>
 
-<div class="flex min-h-screen flex-col">
-	<header class="border-b bg-background px-4">
-		<div class="container mx-auto flex h-16 max-w-7xl items-center justify-between gap-4">
-			<h1 class="text-xl font-semibold">Sentry</h1>
-
-			<div class="flex gap-4">
-				<ModeToggle />
-				<form action="/logout" method="POST">
-					<Button type="submit" variant="destructive"
-						>Logout
-						<LogOut />
-					</Button>
-				</form>
-			</div>
-		</div>
-	</header>
-	<main class="flex-1">
-		{@render children()}
-	</main>
-	<footer class="border-t px-6 py-3 text-center text-sm text-muted-foreground">
-		&copy; {new Date().getFullYear()} Sentry | COSC 111A
-	</footer>
-</div>
+{@render children()}
--- src/routes/(authed)/admin/+page.svelte
+++ src/routes/(authed)/admin/+page.svelte
@@ -1,1 +1,17 @@
+<script lang="ts">
+	import ModeToggle from '$lib/components/ModeToggle.svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { LogOut } from '@lucide/svelte';
+
+	let { children } = $props();
+</script>
+
+<ModeToggle />
+<form action="/logout" method="POST">
+	<Button type="submit" variant="destructive"
+		>Logout
+		<LogOut />
+	</Button>
+</form>
+
 <h1>TODO: Registration</h1>
